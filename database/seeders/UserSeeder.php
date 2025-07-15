2bb6a08eaa0d7d4ddf769f4d5bf366fec7c18be0
Marcel Stadelmann <m@marceli.to>
2025-07-15 11:10:12 +0200

    wip

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,12 +12,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'firstname' => 'Marcel',
-            'name' => 'Stadelmann',
-            'email' => 'm@marceli.to',
-            'password' => \Hash::make('7aq31rr23'),
-            'email_verified_at' => '2025-01-22 18:11:24',
-        ]);
+      User::create([
+        'firstname' => 'Marcel',
+        'name' => 'Stadelmann',
+        'email' => 'm@marceli.to',
+        'password' => \Hash::make('7aq31rr23'),
+        'email_verified_at' => '2025-01-22 18:11:24',
+      ]);
+
+      User::create([
+        'firstname' => 'Andrea',
+        'name' => 'Müller',
+        'email' => 'mueller@pandaundpinguin.ch',
+        'password' => \Hash::make('Fr@U-mU3l-d@sH-$32c'),
+        'email_verified_at' => '2025-01-22 18:11:24',
+      ]);
+
+      User::create([
+        'firstname' => 'Sabina',
+        'name' => 'Sturzenegger',
+        'email' => 'sturzenegger@pandaundpinguin.ch',
+        'password' => \Hash::make('Fr@U-mU3l-d@sH-!48b'),
+        'email_verified_at' => '2025-01-22 18:11:24',
+      ]);
     }
 }
