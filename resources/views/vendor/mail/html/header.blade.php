b3f58c6c2691790b347297bdcb5b41d5c22772f5
Marcel Stadelmann <m@marceli.to>
2025-07-19 19:21:22 +0200

    wip

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,6 @@
 @props(['url'])
 <tr>
   <td class="header">
-    <img src="{{ config('app.url') }}/img/fraumueller.jpg" title="fraumueller.ch" alt="fraumueller.ch" height="49" width="240" style="display:block; height:auto; width: 240px;">
+    <img src="{{ config('app.url') }}/img/fraumueller.png" title="fraumueller.ch" alt="fraumueller.ch" height="49" width="240" style="display:block; height:auto; width: 240px;">
   </td>
 </tr>
