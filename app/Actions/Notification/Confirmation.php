6937350863bc51d16049ba3c5ca59ad34e7a01ef
Marcel Stadelmann <m@marceli.to>
2025-07-19 23:42:17 +0200

    wip

--- app/Actions/Notification/Confirmation.php
+++ app/Actions/Notification/Confirmation.php
@@ -18,7 +18,7 @@ class Confirmation
         ->whereNull('confirmed_at')
         ->where('merchant', 'twint')
         ->whereNotNull('email')
-        ->limit(100)
+        ->limit(3)
         ->get();
 
     foreach ($orders as $order) {
