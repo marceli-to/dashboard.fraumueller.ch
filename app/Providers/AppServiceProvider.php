16d4a612683f11c168b3176532b6498b1cadd0db
Marcel Stadelmann <m@marceli.to>
2025-07-19 23:44:34 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+      Blade::directive('seo_title', function () {
+        return "<?php echo (trim(View::getSection('seo_title')) ? View::getSection('seo_title') . ' • ' : '') . config('seo.title'); ?>";
+      });
 
+      Blade::directive('seo_description', function () {
+        return "<?php echo View::hasSection('seo_description') ? trim(View::getSection('seo_description')) : config('seo.description'); ?>";
+      });
+
+      Blade::directive('seo_image', function () {
+        return "<?php echo View::hasSection('seo_image') ? View::getSection('seo_image') : config('seo.image'); ?>";
+      });
     }
 
     /**
