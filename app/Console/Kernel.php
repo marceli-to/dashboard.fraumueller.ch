52fcf6e42a00a14c9f67477a05c1dd786cabe02a
Marcel Stadelmann <m@marceli.to>
2025-07-28 23:31:58 +0200

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends ConsoleKernel
   protected function schedule(Schedule $schedule): void
   {
     $schedule->command('orders:send-confirmations')->everyMinute();
-    $schedule->command('db:backup')->everyHour();
+    $schedule->command('db:backup')->hourly()->withoutOverlapping();
     // $schedule->command('mail:testmail')->everyMinute();
   }
 
